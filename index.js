8779ede34916410630439909eb1524e97eb2d5f0
krishnajais001 <krishnajais7091@gmail.com>
2024-02-21 01:15:20 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Assignment 1: Array Operations
+// Objective: Practice adding, removing, and updating values in arrays.
+
+let fruits = []; // Creating An Empty Array
+fruits = ["Apple", "Banana", "Orange"]; // Adding Fruits to the array
+fruits.shift(); // Remove the Frist Element
+fruits.push("Grape"); // Adding Fruits to the end
+fruits.splice(1, 1, "Pear"); // Replace the pear to orange
+console.log(fruits);
